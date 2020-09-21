e4c91b58872590f682854b75e43fc29b63fdc72b
Shradha <shashwatishradha5@gmail.com>
2020-09-21 16:23:01 -0600

    More files

---
+++ Flixster/app/src/main/java/com/codepath/flixster/MainActivity.java
@@ -0,0 +1,75 @@
+package com.codepath.flixster;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.codepath.asynchttpclient.AsyncHttpClient;
+import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler;
+import com.codepath.flixster.models.Movie;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import okhttp3.Headers;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String NOW_PLAYING_URL =
+            "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed";
+    public static final String TAG = "MainActivity";
+    RecyclerView rvMovies;
+    MovieAdapter adapter;
+    List<Movie> movies;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        movies = new ArrayList<Movie>();
+        // Lookup the recyclerview in activity layout
+        rvMovies = (RecyclerView) findViewById(R.id.recyclerView);
+        // Create adapter passing in the movie data
+        adapter = new MovieAdapter(this, movies);
+        // Attach the adapter to the recyclerview to populate items
+        rvMovies.setAdapter(adapter);
+        // Set layout manager to position the items
+        rvMovies.setLayoutManager(new LinearLayoutManager(this));
+
+        getNowPlayingMovies();
+        Log.i(TAG, "Movies " + movies.size());
+    }
+
+    private void getNowPlayingMovies(){
+        AsyncHttpClient client = new AsyncHttpClient();
+        client.get(NOW_PLAYING_URL, new JsonHttpResponseHandler() {
+                    @Override
+                    public void onSuccess(int statusCode, Headers headers, JSON response) {
+                        Log.d(TAG, "onSuccess");
+                        JSONObject jsonObject = response.jsonObject;
+                        try {
+                            JSONArray results = jsonObject.getJSONArray("results");
+                            movies.addAll(Movie.fromJsonArray(results));
+                            adapter.notifyDataSetChanged();
+                            Log.i(TAG, "Movies " + movies.size());
+                        } catch (JSONException e) {
+                            Log.e(TAG, "Hit JSON exception", e);
+                            e.printStackTrace();
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(int statusCode, Headers headers, String errorResponse, Throwable t) {
+                        Log.d(TAG, "onFailure");
+                    }
+                }
+        );
+    }
+}
--- Flixster/app/src/main/java/com/codepath/flixster/MovieAdapter.java
+++ Flixster/app/src/main/java/com/codepath/flixster/MovieAdapter.java
@@ -1,4 +1,66 @@
 package com.codepath.flixster;
 
-public class MovieAdapter {
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.codepath.flixster.models.Movie;
+
+import java.util.List;
+
+public class MovieAdapter extends  RecyclerView.Adapter<MovieAdapter.ViewHolder> {
+
+    private Context context;
+    private List<Movie> movieList;
+
+    public MovieAdapter(Context context, List<Movie> movieList) {
+        this.context = context;
+        this.movieList = movieList;
+    }
+
+    @NonNull
+    @Override
+    // Usually involves inflating a layout from XML and returning the holder
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(context);
+
+        View movieView = inflater.inflate(R.layout.item_movie, parent, false);
+
+        ViewHolder viewHolder = new ViewHolder(movieView);
+        return viewHolder;
+    }
+
+    @Override
+    // Involves populating data into the item through holder
+    public void onBindViewHolder(MovieAdapter.ViewHolder holder, int position) {
+        Movie movie = this.movieList.get(position);
+
+        holder.movie_title.setText(movie.getTitle());
+        holder.movie_overview.setText(movie.getOverview());
+    }
+
+    @Override
+    public int getItemCount() {
+        return this.movieList.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        public ImageView movie_poster;
+        public TextView movie_title;
+        public TextView movie_overview;
+
+        public ViewHolder(View movieView){
+            super(movieView);
+            movie_poster = (ImageView) movieView.findViewById(R.id.poster);
+            movie_title = (TextView) movieView.findViewById(R.id.title);
+            movie_overview = (TextView) movieView.findViewById(R.id.overview);
+        }
+    }
 }
+
